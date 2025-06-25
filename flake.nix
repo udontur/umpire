7e829d4332cb331568266bfcd3809de9a89288a0
udontur <h@hadrian.cc>
2025-06-25 23:17:53 +0800

    feat: LETS GOOOOOOOO nix + cmake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,8 @@
 {
   description = "github:udontur/umpire Nix flake";
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  inputs.nix-buildproxy.url = "github:polygon/nix-buildproxy";
   
-  outputs = { self, nixpkgs, nix-buildproxy, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -17,9 +16,22 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ nix-buildproxy.overlays.default ];
         };
-        buildproxy = pkgs.lib.mkBuildproxy ./proxy_content.nix;
+        fmt_src = builtins.fetchTarball{
+          url = "https://github.com/fmtlib/fmt/archive/refs/tags/11.2.0.tar.gz";
+          sha256 = "sha256:0x8j1k1cnmvv5hbhhyfm7bqw2d2rb3jpmz6bc4a195z8pzj582dh";
+        };
+
+        ftxui_src = builtins.fetchTarball{
+          url = "https://github.com/ArthurSonzogni/FTXUI/archive/refs/tags/v6.1.9.tar.gz";
+          sha256 = "sha256:1sbgp1b5v0z8dcfwmgincd759brc1bz64vifdfp4r1afp1672lm6";
+        };
+
+        argparse_src = builtins.fetchTarball{
+          url = "https://github.com/p-ranav/argparse/archive/refs/tags/v3.2.tar.gz";
+          sha256 = "sha256:04wp0q5sxn4hlqzsbq1g99q4k8wn0y1y2ldwld7g3k7yibqi90n3";
+        };
+                  
       in{
         default =
           pkgs.stdenv.mkDerivation rec {
@@ -32,22 +44,36 @@
               cmake
               gnumake
               gcc
-              buildproxy
-              curl
+              git
             ];
 
-            prePatch = ''
-              source ${buildproxy}
+            configurePhase = ''
+              export fmt_src=${fmt_src}
+              export ftxui_src=${ftxui_src}
+              export argparse_src=${argparse_src}
             '';
             
-            cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
+            cmakeFlags = [
+              "-DCMAKE_BUILD_TYPE=Release"
+              "-Dfmt_src=${fmt_src}"
+              "-Dftxui_src=${ftxui_src}"
+              "-Dargparse_src=${argparse_src}"
+            ];
+
+            buildPhase = ''
+              mkdir -p build
+              cd build
+              cmake ..
+              cmake --build .
+            '';
+              
             
             installPhase = ''
               runHook preInstall
               
               mkdir -p $out/bin
-              install -Dm755 ./umpire $out/bin/umpire
-              ln -s $out/bin/umpire $out/bin/um
+              install -Dm755 ./um $out/bin/um
+              ln -s $out/bin/um $out/bin/umpire
 
               runHook postInstall
             '';
